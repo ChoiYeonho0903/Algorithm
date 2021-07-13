e7cefdc16b9b7d6d5581c70b1105b8ff0707ca35
ChoiYeonho0903 <903yh@naver.com>
2021-07-13 23:33:56 +0900

    수리공 항승

---
+++ BAEKJOON_Algorithm/Greedy_1449.java
@@ -0,0 +1,30 @@
+package BAEKJOON_Algorithm;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Greedy_1449 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int L = scanner.nextInt();
+        int link=0;
+        int count=0;
+        int [] leak = new int[N];
+        for (int i = 0; i <leak.length; i++) {
+            leak[i] = scanner.nextInt();
+        }
+        Arrays.sort(leak);
+        link = leak[0];
+        count++;
+        for (int i = 1; i <leak.length ; i++) {
+            if(link+(L-1) >= leak[i])
+                continue;
+            else {
+                count++;
+                link = leak[i];
+            }
+        }
+        System.out.println(count);
+    }
+}
