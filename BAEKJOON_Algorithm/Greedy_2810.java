00edcae97f5863500167f49ccb939a11b80a0384
ChoiYeonho0903 <903yh@naver.com>
2021-07-21 13:30:07 +0900

    컵홀더

---
+++ BAEKJOON_Algorithm/Greedy_2810.java
@@ -0,0 +1,23 @@
+package BAEKJOON_Algorithm;
+
+import java.util.Scanner;
+
+public class Greedy_2810 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int count = 0;
+        int cupholder;
+        String str = scanner.next();
+        for (int i = 0; i <N; i++) {
+            if(str.charAt(i)=='L')
+                count++;
+        }
+        count /= 2;
+        cupholder = N+1-count;
+        if(cupholder>=N)
+            System.out.println(N);
+        else
+            System.out.println(cupholder);
+    }
+}
