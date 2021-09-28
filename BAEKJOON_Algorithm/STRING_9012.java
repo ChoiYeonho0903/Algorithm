a7a6db2299f40c81f377708fbab1881ceadcd541
ChoiYeonho0903 <903yh@naver.com>
2021-09-29 02:14:30 +0900

    괄호

---
+++ BAEKJOON_Algorithm/STRING_9012.java
@@ -0,0 +1,40 @@
+package BAEKJOON_Algorithm;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class STRING_9012 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+        String [] str = new String[T];
+        String [] ans = new String[T];
+        for (int i = 0; i < T; i++) {
+            str[i] = scanner.next();
+            Stack<Character> stack = new Stack<>();
+            for (int j = 0; j <str[i].length(); j++) {
+                if(str[i].charAt(j)==')') {
+                    if(stack.isEmpty()) {
+                        ans[i] = "NO";
+                        break;
+                    }
+                    else {
+                        stack.pop();
+                    }
+                }
+                else if(str[i].charAt(j)=='(') {
+                    stack.push('(');
+                }
+            }
+            if(!stack.isEmpty()) {
+                ans[i] = "NO";
+            }
+            else if(ans[i]==null) {
+                ans[i] = "YES";
+            }
+        }
+        for (int i = 0; i < ans.length; i++) {
+            System.out.println(ans[i]);
+        }
+    }
+}
