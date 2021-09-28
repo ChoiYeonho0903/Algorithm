5a20bb523cc9757a4214b19c6c4797d814905c68
ChoiYeonho0903 <903yh@naver.com>
2021-09-29 02:13:07 +0900

    숫자의 합

---
+++ BAEKJOON_Algorithm/STRING_11720.java
@@ -0,0 +1,18 @@
+package BAEKJOON_Algorithm;
+
+import java.util.Scanner;
+
+public class STRING_11720 {
+    static int N;
+    static String str;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        N = scanner.nextInt();
+        str = scanner.next();
+        int ans = 0;
+        for (int i = 0; i <str.length(); i++) {
+            ans += str.charAt(i) - '0';
+        }
+        System.out.println(ans);
+    }
+}
