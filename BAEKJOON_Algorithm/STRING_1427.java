debbf0765e7be1f69d275f90a3c4197111ee51e5
ChoiYeonho0903 <903yh@naver.com>
2021-09-30 01:49:53 +0900

    소트인사이드

---
+++ BAEKJOON_Algorithm/STRING_1427.java
@@ -0,0 +1,21 @@
+package BAEKJOON_Algorithm;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class STRING_1427 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int i = 0; i <str.length(); i++) {
+            arrayList.add(str.charAt(i)-'0');
+        }
+        Collections.sort(arrayList, Collections.reverseOrder());
+        for (int i = 0; i <arrayList.size(); i++) {
+            System.out.print(arrayList.get(i));
+        }
+        System.out.println("");
+    }
+}
