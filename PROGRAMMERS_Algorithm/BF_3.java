457cca5bc4ac85997c449ba434cf44fa5bc6a100
ChoiYeonho0903 <903yh@naver.com>
2021-10-13 01:55:14 +0900

    카펫

--- PROGRAMMERS_Algorithm/BF_3.java
+++ PROGRAMMERS_Algorithm/BF_3.java
@@ -1,8 +1,12 @@
 package PROGRAMMERS_Algorithm;
 
-public class BF_3 {
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+public class BF_3 {
+    static int brown = 8;
+    static int yellow = 1;
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(brown, yellow)));
     }
     public static int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
