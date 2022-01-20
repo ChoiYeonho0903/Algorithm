c941cc420e2b0a5de541934200a3d288286a807d
ChoiYeonho0903 <903yh@naver.com>
2022-01-20 22:26:19 +0900

    완전탐색

---
+++ BAEKJOON_Algorithm/bruteforce/S3_15649.java
@@ -0,0 +1,46 @@
+package BAEKJOON_Algorithm.bruteforce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class S3_15649 {
+    static StringBuilder sb = new StringBuilder();
+    static int N;
+    static int M;
+    static int[] selected;
+    static boolean used[];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        String str = br.readLine();
+        st = new StringTokenizer(str, " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        selected = new int[M+1];
+        used = new boolean[N+1];
+        rec_func(1);
+        System.out.println(sb);
+    }
+
+    static void rec_func(int k) {
+        if(k==M+1) {
+            for (int i = 1; i <=M; i++) {
+                sb.append(selected[i]).append(" ");
+            }
+            sb.append("\n");
+        } else {
+            for (int i = 1; i <=N; i++) {
+                if(!used[i]) {
+                    selected[k] = i;
+                    used[i] = true;
+                    rec_func(k+1);
+                    selected[k] = 0;
+                    used[i] = false;
+                }
+            }
+        }
+    }
+}
---
+++ BAEKJOON_Algorithm/bruteforce/S3_15651.java
@@ -0,0 +1,38 @@
+package BAEKJOON_Algorithm.bruteforce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class S3_15651 {
+    static StringBuilder sb = new StringBuilder();
+    static int N, M;
+    static int[] selected;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        String str = br.readLine();
+        st = new StringTokenizer(str, " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        selected = new int[N+1];
+        rec_func(1);
+        System.out.println(sb);
+    }
+
+    static void rec_func(int k) {
+        if (k == M+1) {
+            for (int i = 1; i <=M; i++) {
+                sb.append(selected[i]).append(" ");
+            }
+            sb.append("\n");
+        } else {
+            for (int i = 1; i <= N ; i++) {
+                selected[k] = i;
+                rec_func(k+1);
+                selected[k] = 0;
+            }
+        }
+    }
+}
