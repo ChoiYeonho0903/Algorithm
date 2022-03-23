2efbe9f1920c1662679b614c6c58cb12d80144b8
ChoiYeonho0903 <903yh@naver.com>
2022-03-24 00:25:17 +0900

    피보나치 수 5

---
+++ BAEKJOON_Algorithm/dp/B2_10870.java
@@ -0,0 +1,19 @@
+package BAEKJOON_Algorithm.dp;
+
+import java.io.*;
+
+public class B2_10870 {
+    static int N;
+    static int[] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        dp = new int[21];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2; i <= 20; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        System.out.println(dp[N]);
+    }
+}
