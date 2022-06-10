18bf3427f1cc1a830252b50ca02ec62b0e363174
ChoiYeonho0903 <903yh@naver.com>
2022-06-11 03:40:36 +0900

    스택

---
+++ baekjoon/tony/data_structure/DS_10828.java
@@ -0,0 +1,54 @@
+package baekjoon.tony.data_structure;
+
+import java.io.*;
+import java.util.*;
+
+public class DS_10828 {
+    static StringBuilder sb = new StringBuilder();
+    static int N;
+    static Stack<Integer> stack = new Stack<>();
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < N; i++) {
+            String str = br.readLine();
+            st = new StringTokenizer(str, " ");
+            String command = st.nextToken();
+            switch (command) {
+                case "push":
+                    stack.add(Integer.parseInt(st.nextToken()));
+                    break;
+                case "pop":
+                    if(stack.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(stack.pop()).append("\n");
+                    }
+                    break;
+                case "size":
+                    sb.append(stack.size()).append("\n");
+                    break;
+                case "empty":
+                    if(stack.isEmpty()) {
+                        sb.append(1).append("\n");
+                    } else {
+                        sb.append(0).append("\n");
+                    }
+                    break;
+                case "top":
+                    if(stack.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(stack.peek()).append("\n");
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        System.out.print(sb);
+    }
+}
