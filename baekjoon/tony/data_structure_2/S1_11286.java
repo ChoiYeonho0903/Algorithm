495a600c75fd32e5f8cee7f2f20879f30536f6d0
ChoiYeonho0903 <903yh@naver.com>
2022-06-22 21:21:27 +0900

    절대값 힙

---
+++ baekjoon/tony/data_structure_2/S1_11286.java
@@ -0,0 +1,36 @@
+package baekjoon.tony.data_structure_2;
+
+import java.util.*;
+import java.io.*;
+
+public class S1_11286 {
+    static StringBuilder sb = new StringBuilder();
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        PriorityQueue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if(Math.abs(o1) == Math.abs(o2)) {
+                    return o1 - o2;
+                }
+                return Math.abs(o1) - Math.abs(o2);
+            }
+        });
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        for(int i = 0; i < N; i++) {
+            int x = Integer.parseInt(br.readLine());
+            if(x == 0) {
+                if(queue.isEmpty()) {
+                    sb.append(0).append("\n");
+                } else {
+                    sb.append(queue.poll()).append("\n");
+                }
+            } else {
+                queue.add(x);
+            }
+        }
+        System.out.print(sb);
+    }
+}
